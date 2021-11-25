7c90ecb266d85911627f58b612a5c5510b6a7c48
Paul Donskikh <donskikhinvest@gmail.com>
2021-11-25 17:02:13 -0500

    ns-64: NFTCollection FindByContractAddressAndTokenID()

--- store/nft.go
+++ store/nft.go
@@ -10,7 +10,6 @@ import (
 )
 
 type NFTStore interface {
-	// FindByTokenID(ctx context.Context, contractAddr, tokenID string) (*model.NFT, error)
 	Save(ctx context.Context, nft *model.NFT) error
 	FindByContractAddressAndTokenID(ctx context.Context, contractAddress string, tokenID string) (*model.NFT, error)
 }
