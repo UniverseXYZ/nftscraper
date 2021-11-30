63fed18cc7f61f7f2dee77577cb3320d57661799
Paul Donskikh <donskikhinvest@gmail.com>
2021-11-30 09:03:55 -0500

    ns-64: nft_collection_id column

--- db/migration/migrations/0001_init.up.sql
+++ db/migration/migrations/0001_init.up.sql
@@ -12,8 +12,17 @@ CREATE TABLE IF NOT EXISTS transfer (
     log_index bigint
 );
 
+CREATE TABLE IF NOT EXISTS nft_collection (
+    id uuid PRIMARY KEY NOT NULL,
+    contract_addr varchar(42) NOT NULL,
+    "name" varchar(255) NOT NULL,
+    symbol varchar(255) NOT NULL,
+    num_nfts varchar(20) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS nft (
     id uuid PRIMARY KEY NOT NULL,
+    nft_collection_id uuid NOT NULL,
     contract_addr varchar(42) NOT NULL,
     token_id varchar(100) NOT NULL,
     owner_addr varchar(42) NOT NULL,
@@ -22,15 +31,12 @@ CREATE TABLE IF NOT EXISTS nft (
     token_uri text NOT NULL,
     optimized_url text NOT NULL,
     thumbnail_url text NOT NULL,
-    attributes jsonb
-);
-
-CREATE TABLE IF NOT EXISTS nft_collection (
-    id uuid PRIMARY KEY NOT NULL,
-    contract_addr varchar(42) NOT NULL,
-    "name" varchar(255) NOT NULL,
-    symbol varchar(255) NOT NULL,
-    num_nfts varchar(20) NOT NULL
+    attributes jsonb,
+    CONSTRAINT fk_nft_collection
+        FOREIGN KEY (nft_collection_id)
+        REFERENCES nft_collection(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS scraper_cursor (
